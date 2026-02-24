3e21ea9ef163eaeb87df0b87eb0dc4dd480a5944
Agekyan <159093375+Agekyan@users.noreply.github.com>
2026-02-24 13:08:52 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from generators.sample import make_viz
+from generators import set1_alt
 
 def main():
     print("Weber's law test")
-
     make_viz()
+    set1_alt.make_viz()
 
 if __name__ == "__main__":
     main()
