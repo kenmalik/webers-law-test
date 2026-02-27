371b28110730c9247edbc219e279d4121751719a
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2026-02-26 16:57:16 -0800

    Revert "Added set"

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-from generators import set1
-from generators import set1_alt
 from generators.sample import make_viz
 
 def main():
     print("Weber's law test")
-    make_viz()          # sample.py    (..)
-    set1.make_viz()     # set1.py      (6 images, V1-V6)
-    set1_alt.make_viz() # set1_alt.py  (3 images, set1_)
+
+    make_viz()
 
 if __name__ == "__main__":
     main()
