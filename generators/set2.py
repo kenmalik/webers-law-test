75a3a618d91e3bb7524d488b360b5087e2f4596b
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2026-02-27 10:51:11 -0800

    Add horizontal lines graph

--- generators/set2.py
+++ generators/set2.py
@@ -8,15 +8,22 @@ def make_viz():
     _, ax = plt.subplots(figsize=(8, 8))
 
     ax.bar(data.index, data)
-    ax.set_title("Top 10 US Universities International Student Ratios")
+    ax.set_title("Top 15 US Universities International Student Ratios")
+    ax.set_xticks(data.index)
     ax.set_xticklabels(data.index, rotation=45, ha="right")
     ax.set_ylabel("International Student Percentage")
 
     plt.tight_layout()
-    plt.show()
+    plt.savefig("set2-no-aid.png")
+
+    ax.grid(axis="y", linestyle="--", alpha=0.7)
+    plt.savefig("set2-horizontal-lines.png")
+
+    ax.grid(False)
+    plt.savefig("set2-fill-bar.png")
 
 
 def load_data():
     data = pd.read_csv("data/US_Top_50_Universities_2026.csv")
     data.set_index("University_Name", inplace=True)
-    return data.nlargest(10, "National_Rank")["Intl_Student_Ratio"]
+    return data.nlargest(15, "National_Rank")["Intl_Student_Ratio"]
