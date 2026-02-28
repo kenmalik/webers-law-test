00bffce5e53a09ddfc2189f48a35c4de99360c41
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2026-02-27 20:27:28 -0700

    Add set 4

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from generators import set1, set2
+from generators import set1, set2, set4
 from generators import set1_alt
 from generators.sample import make_viz
 
@@ -8,6 +8,7 @@ def main():
     set1.make_viz()     # set1.py      (6 images, V1-V6)
     set1_alt.make_viz() # set1_alt.py  (3 images, set1_)
     set2.make_viz()
+    set4.make_viz()
 
 if __name__ == "__main__":
     main()
