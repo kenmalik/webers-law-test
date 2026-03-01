7b25de61706d50b9a105d21c554882a542619027
Terry8676 <ryan.hern2005@gmail.com>
2026-02-28 21:49:15 -0800

    Update main.py
    deleted call to old line graphs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from generators import set1, set2
 from generators import set1_alt
-from generators import graph1
 from generators import graph1newset
 from generators.sample import make_viz
 
@@ -10,7 +9,6 @@ def main():
     set1.make_viz()     # set1.py      (6 images, V1-V6)
     set1_alt.make_viz() # set1_alt.py  (3 images, set1_)
     set2.make_viz()
-    graph1.make_viz()
     graph1newset.make_viz()
 
 if __name__ == "__main__":
